bbe1462ce7f554b5defe8f46d222669a9e2fc499
Asanbek09 <samudinua@gmail.com>
2026-09-02 11:55:17 +0600

    Ознакомление с ETL и Airflow

---
+++ etl_processes/example/airflow/dags/first_dag.py
@@ -0,0 +1,56 @@
+from etl_processes.example.airflow.dags.first_dag import DAG
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+import logging
+
+from airflow.sensors.time_delta import TimeDeltaSensor
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator  # Исправлено: operators (без "os") и python вместо python_operator
+
+DEFAULT_ARGS = {
+    'start_date': days_ago(12),
+    'owner': 'Me',
+    'poke_interval': 600
+}
+
+dag = DAG(
+    "simple_dag", 
+    schedule_interval='@daily', 
+    default_args=DEFAULT_ARGS, 
+    max_active_runs=1, 
+    tags=['me'],
+    catchup=False  # Добавлено, чтобы не запускать все пропущенные дни
+)
+
+wait_until_6am = TimeDeltaSensor(
+    task_id='wait_until_6am',
+    delta=timedelta(seconds=6*60*60),
+    dag=dag
+)
+
+echo_ds = BashOperator(
+    task_id='echo_ds',
+    bash_command='echo {{ ds }}',
+    dag=dag
+)
+
+def first_func():
+    logging.info("First Log")
+
+first_task = PythonOperator(
+    task_id='first_task',
+    python_callable=first_func,
+    dag=dag
+)
+
+# 👇 СОЗДАЕМ second_task
+def second_func():
+    logging.info("Second Log")
+
+second_task = PythonOperator(
+    task_id='second_task',
+    python_callable=second_func,
+    dag=dag
+)
+
+wait_until_6am >> echo_ds >> [first_task, second_task]
---
+++ etl_processes/example/dag.md
@@ -0,0 +1,6 @@
+dag.doc_md = __doc__
+
+wait_until_6am.doc_md = """Сенсор. Ждет наступления 6am по Гринвичу"""
+echo_ds.doc_md = """Пишет в лог exectuion_date"""
+first_task.doc_md = """Пишет в лог 'First Log'"""
+second_task.doc_md = """Пишет в лог 'Second Log'"""
